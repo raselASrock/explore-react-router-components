1309ab50a80b1d5ef6a9a95baa413e3c7272be0b
raselASrock <mdraselm413@gmail.com>
2022-10-06 01:36:57 +0600

    brouser dom

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Products from './Components/Products';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+        <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
       </Routes>
