dc8a8f285a00867859907092ab96fb7e32f1958b
raselASrock <mdraselm413@gmail.com>
2022-10-06 01:28:41 +0600

    component

---
+++ src/Components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>Lots Of Products Here.</h2>
+        </div>
+    );
+};
+
+export default Products;
